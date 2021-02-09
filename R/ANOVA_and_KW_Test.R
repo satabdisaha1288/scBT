352076a5129e56962175aa5dd137ed50e4a0a0f9
Satabdi Saha <satabdisaha1288@gmail.com>
2021-02-09 15:44:09 -0500

    Analysis script ANOVA KW
    Check performance of KW test and ANOVA test

--- R/ANOVA_and_KW_Test.R
+++ R/ANOVA_and_KW_Test.R
@@ -1,36 +1,21 @@
-my_data_summary<-rep(list(data.frame()),ncol(simulated.data_dose)-1)
-#Look at summary statistics for each gene by group
-for(i in 1: (ncol(simulated.data_dose)-1))
-{
-  my_data<-data.frame(simulated.data_dose[,i],as.factor(simulated.data_dose[,ncol(simulated.data_dose)]))
-  colnames(my_data)<-c("value","dose")
-  library(dplyr)
-  my_data_summary[[i]]<-group_by(my_data, dose) %>%
-    summarise(
-      count = n(),
-      mean = mean(value, na.rm = TRUE),
-      sd = sd(value, na.rm = TRUE)
-    )
-}
+
 #Conduct the ANOVA and Kruskal-Wallis Test for each gene
-my_data_anova<-rep(list(list()),ncol(simulated.data_dose)-1)
-my_data_anova_p_value<-vector()
-my_data_kruskal_p_value<-vector()
-for(i in 1: (ncol(simulated.data_dose)-1))
-{
-  my_data<-data.frame(simulated.data_dose[,i],as.factor(simulated.data_dose[,ncol(simulated.data_dose)]))
-  colnames(my_data)<-c("value","dose")
-  res.aov <- aov(value ~ dose, data = my_data)
-  my_data_anova[[i]]<-summary.aov(res.aov)
-  my_data_anova_p_value[i]<-my_data_anova[[i]][[1]][["Pr(>F)"]][1]
-  res.kruskal<-kruskal.test(value ~ dose, data = my_data)
-  my_data_kruskal_p_value[i]<-res.kruskal$p.value
-}
-my_data_anova_pred_dec<-ifelse(my_data_anova_p_value < 0.01, "Positive","Negative")
-my_data_kruskal_pred_dec<-ifelse(my_data_kruskal_p_value < 0.01, "Positive","Negative")
-length(which(my_data_anova_pred_dec == "Negative"))
-length(which(my_data_kruskal_pred_dec == "Negative"))
+source('~/Documents/Documents/Summer_2019/Mouse_Liver/Rance/snSeq-DGE/R/anova_test.R')
+source('~/Documents/Documents/Summer_2019/Mouse_Liver/Rance/snSeq-DGE/R/KW_test.R')
+#Here data is a dataframe of n cells as rows and p+1 columns with first p columns as p genes and the last 
+#column is the dose information
+mydata<-simulated.data_dose
+my_data_kruskal_p_value<-KW_test(simulated.data_dose)
+my_data_anova_p_value<-anova_test(simulated.data_dose)
+my_data_anova_pred_dec<-ifelse(my_data_anova_p_value < 0.05, "Positive","Negative")
+my_data_kruskal_pred_dec<-ifelse(my_data_kruskal_p_value < 0.05, "Positive","Negative")
 my_data_true_dec<-ifelse(simulated.gene.metadata$Direction == "Upregulated" | 
                            simulated.gene.metadata$Direction == "Downregulated" , "Positive","Negative")
+
+#Generate the confusion matrix for the ANOVA and KW test wrt the reference data
+caret::confusionMatrix(factor(my_data_anova_pred_dec,levels=c("Positive","Negative")),
+                       factor(my_data_true_dec,levels=c("Positive","Negative")))
+caret::confusionMatrix(factor(my_data_kruskal_pred_dec,levels=c("Positive","Negative")),
+                       factor(my_data_true_dec,levels=c("Positive","Negative")))
 
 
