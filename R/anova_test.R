8f32505d3044f4e2097b0fd20d359fb3aa2387d7
Nault, Rance <naultran@msu.edu>
2021-04-08 21:47:08 -0400

    Updated the ANOVA test to handle single-cell experiment objects and increase speed using apply

--- R/anova_test.R
+++ R/anova_test.R
@@ -1,3 +1,38 @@
+#' Performs a genewise ANOVA test on a SingleCellExperiment object
+#' 
+#' @param sce SingleCellExperiment object with a logcounts assay 
+#' and Dose column in the cell metadata
+#' 
+#' @return a vector of p values from the ANOVA test
+#' 
+#' @example 
+#' 
+#' @importFrom SingleCellExperiment logcounts colData
+#' @export
+batchANOVA = function(sce){
+  data = as.matrix(logcounts(sce))
+  dose = colData(sce)$Dose
+  aov.pvalues = apply(data, 1, function(x) runAnova(x, dose))
+  return(aov.pvalues)
+}
+
+runAnova = function(data, dose){
+  my_data = data.frame(value = data, dose = dose)
+  res.aov = aov(value ~ dose, data = my_data)
+  aov.pvalue = summary(res.aov)[[1]][["Pr(>F)"]][1]
+  return(aov.pvalue)
+}
+
+
+#' Performed genewise ANOVA test on a cell x row matrix
+#' 
+#' @param data matrix where cells are rows and genes are columns
+#' 
+#' @return
+#' 
+#' @example 
+#' 
+#' @export
 #Function for ANOVA test for each gene 
 #data is a dataframe of n cells as rows and p+1 columns with first p columns as p genes and the last 
 #column is the dose information
@@ -14,3 +49,4 @@ anova_test<-function(data){
   }
   return(my_data_anova_p_value)
 }
+
