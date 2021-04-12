fa30ba7b49d21548cbc5fa5ca0e48c2c7c24b007
Jack <dodsonj9901@gmail.com>
2021-04-12 12:46:22 -0400

    Reworked kw)test function and added documentatation
    NOT TESTED

--- R/KW_test.R
+++ R/KW_test.R
@@ -1,3 +1,54 @@
+# Author: Jack Dodson
+
+#' Performs a genewise Kruskal Wallis test on a SingleCellExperiment object
+#' 
+#' @param sce SingleCellExperiment object with a logcounts assay 
+#' and Dose column in the cell metadata
+#' 
+#' @return a vector of p values from the Kruskal Wallis test
+#' 
+#' @example 
+#' 
+#' @importFrom SingleCellExperiment logcounts colData
+#' @export
+batchKW = function(sce){
+  data = as.matrix(logcounts(sce))
+  dose = colData(sce)$Dose
+  kw.pvalues = apply(data, 1, function(x) runKW(x, dose))
+  return(kw.pvalues)
+}
+
+#' Performs a Kruskal Wallis test on a logcounts vactor for a given dose
+#' 
+#' @param data The logcounts vector
+#' @param dose The dose to analyze
+#' 
+#' @return A p value from the Kruskal Wallis test
+#' 
+#' @example 
+#' 
+#' @importFrom
+#' @export
+runKW = function(data, dose){
+  my_data <- data.frame(value = data, dose = dose)
+  res.kw = kruskal.test(value ~ dose, data = my_data)
+  kw.pvalue = summary(res.kw)[[1]][["Pr(>F)"]][1]
+  return(kw.pvalue)
+}
+
+##########################################################################
+
+# Author: Satabdi Saha
+
+#' Performed genewise Kruskal Wallis test on a cell x row matrix
+#' 
+#' @param data matrix where cells are rows and genes are columns
+#' 
+#' @return
+#' 
+#' @example 
+#' 
+#' @export
 #Function for carrying out the Kruskal Wallis Test
 #data is a dataframe of n cells as rows and p+1 columns with first p columns as p genes and the last 
 #column is the dose information
@@ -12,3 +63,5 @@ KW_test<-function(data){
   }
   return(my_data_kruskal_p_value)
 }
+
+test_result <- KW_test(KW_input)
