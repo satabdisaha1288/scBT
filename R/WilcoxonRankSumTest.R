c349172082e6184352f9dcfcbcb4913e733a080e
Jack <dodsonj9901@gmail.com>
2021-04-15 11:47:02 -0400

    Created batchWRS and runWRS that accept SingleCellExperiment objects
    Tested on synthetic data -- not validated

--- R/WilcoxonRankSumTest.R
+++ R/WilcoxonRankSumTest.R
@@ -1,3 +1,41 @@
+# Author: Jack Dodson
+
+#' Performs a genewise Wilcoxon Rank Sum test on a SingleCellExperiment object
+#' 
+#' @param sce SingleCellExperiment object with a logcounts assay 
+#' and Dose column in the cell metadata
+#' 
+#' @return a vector of p values from the Wilcoxon Rank Sum test
+#' 
+#' @example 
+#' 
+#' @importFrom SingleCellExperiment logcounts colData
+#' @export
+batchWRS = function(sce){
+  data = as.matrix(logcounts(sce))
+  dose = colData(sce)$Dose
+  wrs.pvalues = apply(data, 1, function(x) runWRS(x, dose))
+  return(wrs.pvalues)
+}
+
+#' Performs a Wilcoxon Rank Sum test on a logcounts vactor for a given dose
+#'
+#' @param data The logcounts vector
+#' @param dose The dose to analyze
+#'
+#' @return A p value from the Wilcoxon Rank Sum test
+#'
+#' @example
+#'
+#' @importFrom
+#' @export
+runWRS = function(data, dose){
+  my_data <- data.frame(value = data, dose = dose)
+  res.wrs = wilcox.test(value ~ dose, data = my_data)
+  wrs.pvalue = res.wrs[[3]]
+  return(wrs.pvalue)
+}
+
 ##########################################################################
 
 # Author: Satabdi Saha
