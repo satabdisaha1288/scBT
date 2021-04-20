bb2865ecf97b063a94d57636dc804c61f71d1fea
Satabdi Saha <satabdisaha1288@gmail.com>
2021-04-20 15:39:17 -0400

    Update calc_prior_hyperparameters.R

--- R/calc_prior_hyperparameters.R
+++ R/calc_prior_hyperparameters.R
@@ -1,7 +1,7 @@
 #Calculate prior hyper-parameters
 
 #Calculate a_sigma, b_sigma
-function(sigma_mean,sigma_var){
+calc_a_w_b_w<-function(sigma_mean,sigma_var){
   mean_sigma_mean<-mean(sigma_mean)
   mean_sigma_var<-mean(sigma_var)
   a_sigma<-(1/((mean_sigma_mean^2)*mean_sigma_var)) + 2
@@ -11,7 +11,7 @@ function(sigma_mean,sigma_var){
   return(output)
 }
 #Calculate a_w,b_w
-function(omega_mean, omega_var){
+calc_a_sigma_a_sigma<-function(omega_mean, omega_var){
   mean_omega_mean<-mean(omega_mean)
   mean_omega_var<-mean(omega_var)
   a_w = round((((mean_omega_mean^2)*(1-mean_omega_mean)) / mean_omega_var) - mean_omega_mean ,2)
