16b15eee54dc08427766e486153e6ac402f048e7
Satabdi Saha <satabdisaha1288@gmail.com>
2021-04-20 11:52:34 -0400

    Create Chi-square-test-of-indepence-geneExp-DoseLevel.R

---
+++ R/Chi-square-test-of-indepence-geneExp-DoseLevel.R
@@ -0,0 +1,50 @@
+#' Chi-square test of independence for binned gene expression data
+#' @author Satabdi Saha
+#' @param sce A Single Cell Object 
+#' @return FDR adjusted p-values 
+#
+#' @export
+#'
+#' @examples
+runChi <- function(sce){
+  data_logcounts = as.matrix(logcounts(sce))
+  dose = colData(sce)$Dose
+  data_dose<-data.frame(t(data_logcounts),dose)
+  colnames(data_dose)<-c(rownames(data_logcounts),"Dose")
+  #Break the dataframe to data lists for seperate dose groups
+  split_tibble <- function(tibble, column = 'col') {
+    tibble %>% dplyr::split(., .[,column]) %>% lapply(., function(x) x[,setdiff(names(x),column)])
+  }
+  data = dplyr::split_tibble(data_dose,"Dose")
+  data<-lapply(data, function(x) t(x))
+  max_value_data<-max(sapply(data, function(x) apply(x,1,max)))
+  #Create a data list with genes binned into certain values
+  data_bin<-lapply(data, function(x) t(apply(x,1,function(x){
+    new_breaks<-c(-0.01,0.5,1,2,3,4,ceiling(max_value_data))
+    table(cut(x, new_breaks))
+  })))
+  #Create a list of data frames for applying chi-square test of independence
+  df<-rep(list(data.frame()),length=nrow(data_bin[["0"]]))
+  names(df)<-nrow(data_bin[["0"]])
+  for(j in 1:nrow(data_bin[["0"]]))
+  {
+    a1 <- rapply(data_bin, classes = 'matrix', how = 'list', f = function(x) x[j, , drop = FALSE]) 
+    # a second `lapply` is required to drop `NULL` entries
+    a1Only <- lapply(a1, Filter, f = Negate(is.null))
+    df[[j]] <- data.frame(matrix(unlist(a1Only), nrow=length(a1Only), byrow=TRUE))
+    rownames(df[[j]])<-levels(dose_level)
+    colnames(df[[j]])<-colnames(data_bin[["0.01"]])
+  }
+  chisq_test<-lapply(df,function(x) chisq.test(x[,which(colSums(x)>0)]))
+  names(chisq_test)<-rownames(data[["0"]])
+  chisq_test_pvalue_adj<-p.adjust(sapply(chisq_test,function(x) x$p.value),method = "fdr")
+  return(chisq_test_pvalue_adj)
+}
+
+
+
+
+
+
+
+
