60d15b97bccffffd3c372a86c7ef114d789e12f2
Jack Dodson <dodsonj9901@gmail.com>
2021-04-21 18:53:38 -0400

    Added documentation to calc_a_sigma_b_sigma and calc_a_w_b_w
    Also ran devtools::document() to add these functions to NAMESPACE

--- R/calc_prior_hyperparameters.R
+++ R/calc_prior_hyperparameters.R
@@ -1,7 +1,10 @@
-#Calculate prior hyper-parameters
-
-#Calculate a_sigma, b_sigma
-
+#' Calculate a_sigma, b_sigma
+#' @author Satabdi Saha
+#' @param sigma_mean INSERT DESCR HERE
+#' @param sigma_var INSERT DESCR HERE
+#' @return a_sigma and b_sigma values
+#
+#' @export
 calc_a_sigma_b_sigma<-function(sigma_mean,sigma_var){
   mean_sigma_mean<-mean(sigma_mean)
   mean_sigma_var<-mean(sigma_var)
@@ -11,7 +14,14 @@ calc_a_sigma_b_sigma<-function(sigma_mean,sigma_var){
   names(output)<-c("a_sigma","b_sigma")
   return(output)
 }
-#Calculate a_w,b_w
+
+#' Calculate a_w, b_w
+#' @author Satabdi Saha
+#' @param omega_mean INSERT DESCR HERE
+#' @param omega_var INSERT DESCR HERE
+#' @return a_w and b_w values
+#
+#' @export
 calc_a_w_b_w<-function(omega_mean, omega_var){
   mean_omega_mean<-mean(omega_mean)
   mean_omega_var<-mean(omega_var)
