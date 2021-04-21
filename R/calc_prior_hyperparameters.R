134831e51723045b48198c28dc666570d34839f2
Jack Dodson <dodsonj9901@gmail.com>
2021-04-21 19:05:12 -0400

    Added documentation back in for calc_prior_hyperparameters.R

--- R/calc_prior_hyperparameters.R
+++ R/calc_prior_hyperparameters.R
@@ -1,4 +1,10 @@
-#Calculate a_sigma, b_sigma
+#' Calculate a_sigma, b_sigma
+#' @author Satabdi Saha
+#' @param sigma_mean INSERT DESCR HERE
+#' @param sigma_var INSERT DESCR HERE
+#' @return a_sigma and b_sigma values
+#
+#' @export
 calc_a_sigma_b_sigma<-function(sigma_mean,sigma_var){
   mean_sigma_mean<-mean(sigma_mean)
   mean_sigma_var<-mean(sigma_var)
@@ -9,7 +15,13 @@ calc_a_sigma_b_sigma<-function(sigma_mean,sigma_var){
   return(output)
 }
 
-#Calculate a_w,b_w
+#' Calculate a_w, b_w
+#' @author Satabdi Saha
+#' @param omega_mean INSERT DESCR HERE
+#' @param omega_var INSERT DESCR HERE
+#' @return a_w and b_w values
+#
+#' @export
 calc_a_w_b_w<-function(omega_mean,omega_var)
   mean_omega_mean<-mean(omega_mean)
   mean_omega_var<-mean(omega_var)
