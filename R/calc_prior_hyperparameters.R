db6aba9c1dfb3ed386536b45dd8c7cfda5639530
Satabdi Saha <satabdisaha1288@gmail.com>
2021-04-21 16:47:49 -0400

    Update calc_prior_hyperparameters.R

--- R/calc_prior_hyperparameters.R
+++ R/calc_prior_hyperparameters.R
@@ -2,17 +2,19 @@
 
 #Calculate a_sigma, b_sigma
 
-calc_a_sigma_b_sigma<-function(sigma_mean,sigma_var){
-  mean_sigma_mean<-mean(sigma_mean)
-  mean_sigma_var<-mean(sigma_var)
-  a_sigma<-(1/((mean_sigma_mean^2)*mean_sigma_var)) + 2
-  b_sigma<-1/(mean_sigma_mean* (a_sigma-1))
+calc_a_sigma_b_sigma<-function(sce){
+  data<-logcounts(sce)
+  sigma<- apply(data, 1, var)
+  mean_sigma<-mean(sigma[sigma>0])
+  var_sigma<-sd(sigma[sigma>0])^2
+  a_sigma<-(1/((mean_sigma^2)*var_sigma)) + 2
+  b_sigma<-1/(mean_sigma* (a_sigma-1))
   output<-c(a_sigma,b_sigma)
   names(output)<-c("a_sigma","b_sigma")
   return(output)
 }
 #Calculate a_w,b_w
-calc_a_w_b_w<-function(omega_mean, omega_var){
+calc_a_w_b_w<-function(omega_mean,omega_var)
   mean_omega_mean<-mean(omega_mean)
   mean_omega_var<-mean(omega_var)
   a_w = round((((mean_omega_mean^2)*(1-mean_omega_mean)) / mean_omega_var) - mean_omega_mean ,2)
