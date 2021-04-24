b1a742f9ad3393dd84a0df5acc573a78c762029a
Nault, Rance <naultran@msu.edu>
2021-04-24 09:36:11 -0400

    created the function to extract DEGs

--- R/Main.R
+++ R/Main.R
@@ -44,3 +44,33 @@ DETest = function(sce, method = "All", verbose = FALSE){
   }
   return(DETest.list)
 }
+
+
+getDEGs = function(sce, DETestoutput, threshold = 0.05, bayes.threshold = 1/3, fc.threshold = 0, pct.expressed = 0, verbose = TRUE){
+  fc = abs(calcFC(sce))
+  fc.max = data.frame(apply(fc, 1, function(x) max(x)))
+  pz = calcZeroP(sce)
+  pz.min = data.frame(apply(pz, 1, function(x) min(x)))
+  
+  DEGenes.list = list()
+  
+  for (test in names(DETestoutput)){
+    if (verbose) {message(paste0("Evaluating assay:", test))}
+    filtered = NULL
+    #How to deal with LRT_linear and Bayes?
+    if (ncol(DETestoutput[[test]]) == 1){
+      merged.df = Reduce(merge, lapply(list(DETestoutput[[test]], fc.max, pz.min), function(x) data.frame(x, rn = row.names(x))))
+      colnames(merged.df) = c("Gene", "pvalue", "fc.max", "pz.min")
+      filtered = merged.df %>% filter(pvalue <= threshold & fc.max >= fc.threshold & pz.min >= pct.expressed)
+    } else if (test != 'LRTLin' & test != 'Bayes'){
+      stopifnot(identical(dim(DETestoutput[[test]]), dim(fc)) == TRUE)
+      DETestoutput[[test]][which(fc < fc.threshold)] = NA
+      min.pval = data.frame(apply(DETestoutput[[test]], 1, function(x) min(x)))
+      merged.df = Reduce(merge, lapply(list(min.pval, fc.max, pz.min), function(x) data.frame(x, rn = row.names(x))))
+      colnames(merged.df) = c("Gene", "pvalue", "fc.max", "pz.min")
+      filtered = merged.df %>% filter(pvalue <= threshold & fc.max >= fc.threshold & pz.min >= pct.expressed)
+    }
+    DEGenes.list[[test]] = filtered
+  }
+  return(DEGenes.list)
+}
