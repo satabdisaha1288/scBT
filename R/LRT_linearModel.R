bd8870ac60dc3bc4b525c1190ef0d8c61de4e031
Jack Dodson <dodsonj9901@gmail.com>
2021-04-28 10:49:44 -0400

    Renamed LRT_linearModel_reworked to LRT_linearModel and removed old functions

--- R/LRT_linearModel.R
+++ R/LRT_linearModel.R
@@ -1,48 +1,33 @@
-#' Linear Model LRT for dose dependent differential gene expression
-#' @author Satabdi Saha, Jack Dodson
-#' @param sce A single cell object (Does not allow a logcount matrix with genes having
-#' a dropout of 100%. Such genes should be filtered out before running the test)
+#' Title Reworked version of LRT_linearModel.R (no-loops)
+#' @author Satabdi
+#' @param sce SingleCellExperiment object with a logcounts assay 
+#' and Dose column in the cell metadata
 #'
-#' @return A data frame giving the test statistic and unadjusted p values
-#' for the linear, logistic and combined regression models for p genes
-#' @export
-batchLRT_linearModel <- function(sce){
-  result = data.frame()
-  for(i in 1:nrow(logcounts(sce))){
-    data = as.matrix(logcounts(sce)[i,])
-    if(sum(data == 0) != nrow(data)){
-      output <- runLRT_linearModel(data)
-      row_stats <- c(i, output[1,1], output[2,1], output[1,2],
-                     output[2,2], output[1,3], output[2,3])
-      result <- rbind(result, row_stats)
-    }
-  }
-  colnames(result)<-c("gene", "teststat_norm", "pvalue_norm",
-                      "teststat_logistic", "pvalue_logistic",
-                      "teststat_comb", "pvalue_comb")
-  return(result)
-}
-
-#' Linear Model LRT for dose dependent differential gene expression
-#' @author Satabdi Saha
-#' @param sce A single cell object (Does not allow a logcount matrix with genes having
-#' a dropout of 100%. Such genes should be filtered out before running the test)
-#'
-#' @return A data frame giving the test statistic and unadjusted p values
-#' for the linear, logistic and combined regression models for p genes
-#' @export
-runLRT_linearModel<-function(x){
-  fit.logistic<-glm(ifelse(x >0,1,0)~1,family = binomial)
-  fit.logistic.alt<-glm(ifelse(x >0,1,0) ~ 
-                          as.numeric(as.character(dose)),family = binomial)
-  fit.mean<- lm(x[x>0]~1)
-  fit.mean.alt<-lm(x[x>0] ~ as.numeric(as.character(dose)[x >0]))
-  loglik_normal<-(as.numeric(logLik(fit.mean))-as.numeric(logLik(fit.mean.alt)))
-  loglik_logistic<-(as.numeric(logLik(fit.logistic))-as.numeric(logLik(fit.logistic.alt)))
-  resultvec <- c(-2*loglik_normal, pchisq(-2*loglik_normal, df = 3-2, lower.tail = FALSE),
-                 -2*loglik_logistic, pchisq(-2*loglik_logistic, df = 2-1, lower.tail = FALSE),
-                 -2 * (loglik_normal + loglik_logistic), 
-                 pchisq(-2 * (loglik_normal + loglik_logistic), df = 2, lower.tail = FALSE))
-  result <- matrix(resultvec, nrow=2, ncol=3, dimnames=list(metric=c('lrstat', 'p.value'), component=c('norm', 'binom', 'comb')))
-  return(result)
+#' @return A dataframe having the test statistic and p-values 
+#' @export 
+LRT_linearModel_reworked<-function(sce){
+  data = as.matrix(logcounts(sce))
+  dose = as.numeric(as.character(colData(sce)$Dose))
+  mydf<-data.frame(t(data),dose)
+  # Specify the columns that contain your predictor variables
+  respIdx <- seq(1:(ncol(mydf)-1));
+  # lm(y ~ x), for x being a single predictor
+  output<-lapply(respIdx[-which(colSums(mydf)==0)], function(x) {
+    fit.logistic<-glm(ifelse(mydf[,x] >0,1,0)~1,family = binomial)
+    fit.logistic.alt<-glm(ifelse(mydf[,x] >0,1,0) ~ mydf[, ncol(mydf)],family = binomial)
+    fit.mean<- lm(mydf[,x][mydf[,x]>0]~1)
+    fit.mean.alt<-lm(mydf[,x][mydf[,x]>0] ~ mydf[, ncol(mydf)][mydf[,x]>0])
+    loglik_normal<-(as.numeric(logLik(fit.mean))-as.numeric(logLik(fit.mean.alt)))
+    loglik_logistic<-(as.numeric(logLik(fit.logistic))-as.numeric(logLik(fit.logistic.alt)))
+    resultvec <- c(-2*loglik_normal, pchisq(-2*loglik_normal, df = 3-2, lower.tail = FALSE),
+                   -2*loglik_logistic, pchisq(-2*loglik_logistic, df = 2-1, lower.tail = FALSE),
+                   -2 * (loglik_normal + loglik_logistic), 
+                   pchisq(-2 * (loglik_normal + loglik_logistic), df = 2, lower.tail = FALSE))
+    names(resultvec) <- c("lrstat_norm",'p.value.norm',"lrstat_binom",'p.value.binom',"lrstat_comb",'p.value.comb')
+    return(resultvec)
+  })
+  output<-data.frame(matrix(unlist(output), nrow=length(output), byrow=TRUE))
+  colnames(output)<-c("lrstat_norm",'p.value.norm',"lrstat_binom",'p.value.binom',"lrstat_comb",'p.value.comb')
+  rownames(output)<-rownames(logcounts(sce))[-which(rowSums(logcounts(sce))==0)]
+  return(output) 
 }
