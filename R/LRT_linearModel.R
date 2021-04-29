43bd48a2128b2d64bfcd5b6a38d26dbcf30308ed
Nault, Rance <naultran@msu.edu>
2021-04-29 17:25:46 -0400

    major fixes on the LRT code to prevent errors from the lapply function

--- R/LRT_linearModel.R
+++ R/LRT_linearModel.R
@@ -4,21 +4,23 @@
 #' and Dose column in the cell metadata
 #'
 #' @return A dataframe having the test statistic and p-values 
-#' @export 
-LRT_linearModel<-function(sce){
+old_LRT_linearModel<-function(sce){
   data = as.matrix(logcounts(sce))
   dose = as.numeric(as.character(colData(sce)$Dose))
   mydf<-data.frame(t(data),dose)
+  
   # Specify the columns that contain your predictor variables
-  respIdx <- seq(1:(ncol(mydf)-1));
+  respIdx <- seq(1:(ncol(mydf)-1))
+  
   # lm(y ~ x), for x being a single predictor
-  output<-lapply(respIdx[-which(colSums(mydf)==0)], function(x) {
-    fit.logistic<-glm(ifelse(mydf[,x] >0,1,0)~1,family = binomial)
-    fit.logistic.alt<-glm(ifelse(mydf[,x] >0,1,0) ~ mydf[, ncol(mydf)],family = binomial)
-    fit.mean<- lm(mydf[,x][mydf[,x]>0]~1)
-    fit.mean.alt<-lm(mydf[,x][mydf[,x]>0] ~ mydf[, ncol(mydf)][mydf[,x]>0])
-    loglik_normal<-(as.numeric(logLik(fit.mean))-as.numeric(logLik(fit.mean.alt)))
-    loglik_logistic<-(as.numeric(logLik(fit.logistic))-as.numeric(logLik(fit.logistic.alt)))
+  output <- lapply(respIdx[which(colSums(mydf) > 0)], function(x) {
+    fit.logistic <- glm(ifelse(mydf[,x] > 0, 1, 0)~1, family = binomial)
+    fit.logistic.alt <- glm(ifelse(mydf[,x] > 0, 1, 0) ~ mydf[, ncol(mydf)],
+                            family = binomial)
+    fit.mean <- lm(mydf[,x][mydf[,x] > 0] ~ 1)
+    fit.mean.alt <- lm(mydf[,x][mydf[,x] > 0] ~ mydf[, ncol(mydf)][mydf[,x] > 0])
+    loglik_normal <- (as.numeric(logLik(fit.mean)) - as.numeric(logLik(fit.mean.alt)))
+    loglik_logistic <- (as.numeric(logLik(fit.logistic)) - as.numeric(logLik(fit.logistic.alt)))
     resultvec <- c(-2*loglik_normal, pchisq(-2*loglik_normal, df = 3-2, lower.tail = FALSE),
                    -2*loglik_logistic, pchisq(-2*loglik_logistic, df = 2-1, lower.tail = FALSE),
                    -2 * (loglik_normal + loglik_logistic), 
@@ -30,4 +32,44 @@ LRT_linearModel<-function(sce){
   colnames(output)<-c("lrstat_norm",'p.value.norm',"lrstat_binom",'p.value.binom',"lrstat_comb",'p.value.comb')
   rownames(output)<-rownames(logcounts(sce))[-which(rowSums(logcounts(sce))==0)]
   return(output) 
+}
+
+#' Title Reworked version of LRT_linearModel.R (no-loops)
+#' @author Satabdi
+#' @param sce SingleCellExperiment object with a logcounts assay 
+#' and Dose column in the cell metadata
+#'
+#' @return A dataframe having the test statistic and p-values 
+#' @export 
+LRT_linearModel<-function(sce){
+  data = as.matrix(logcounts(sce))
+  dose = as.numeric(as.character(colData(sce)$Dose))
+  
+  #filter zero exp?
+  
+  output = apply(data, 1, function(x) runLRT_Linear(x, dose))
+  output = t(output)
+}
+
+
+#' Title Reworked version of LRT_linearModel.R (no-loops)
+#' @author Satabdi
+#' @param sce SingleCellExperiment object with a logcounts assay 
+#' and Dose column in the cell metadata
+#'
+#' @return A dataframe having the test statistic and p-values 
+#' @export 
+runLRT_Linear = function(data, dose){
+  fit.logistic <- glm(ifelse(data > 0, 1, 0) ~ 1, family = binomial)
+  fit.logistic.alt <- glm(ifelse(data > 0, 1, 0) ~ dose, family = binomial)
+  fit.mean <- lm(data[which(data > 0)] ~ 1)
+  fit.mean.alt <- lm(data[which(data > 0)] ~ dose[which(data > 0)])
+  loglik_normal <- (as.numeric(logLik(fit.mean)) - as.numeric(logLik(fit.mean.alt)))
+  loglik_logistic <- (as.numeric(logLik(fit.logistic)) - as.numeric(logLik(fit.logistic.alt)))
+  resultvec <- c(-2*loglik_normal, pchisq(-2*loglik_normal, df = 3-2, lower.tail = FALSE),
+                 -2*loglik_logistic, pchisq(-2*loglik_logistic, df = 2-1, lower.tail = FALSE),
+                 -2 * (loglik_normal + loglik_logistic), 
+                 pchisq(-2 * (loglik_normal + loglik_logistic), df = 2, lower.tail = FALSE))
+  names(resultvec) <- c("lrstat_norm",'p.value.norm',"lrstat_binom",'p.value.binom',"lrstat_comb",'p.value.comb')
+  return(resultvec)
 }
--- R/statUtils.R
+++ R/statUtils.R
@@ -6,53 +6,51 @@
 #' @return INSERT DESCRIPTION
 #' 
 #' @export
-runMAST = function(sce){
-  library(MAST)
-  scaRaw = FromMatrix(as.matrix(logcounts(sce)),
+runMAST <- function(sce){
+  scaRaw <- FromMatrix(as.matrix(logcounts(sce)),
                       data.frame(colData(sce)),
                       data.frame(rowData(sce))
   )
   cdr <-colSums(assay(scaRaw) > 0)
-  colData(scaRaw)$cngeneson = scale(cdr)
-  filterCrit = with(colData(scaRaw),cngeneson > 1)
-  rowData(scaRaw)$symbolid = rownames(rowData(sce))
-  sca = subset(scaRaw,filterCrit)
-  Dose = factor(colData(scaRaw)$Dose)
-  Dose = relevel(Dose,"0")
+  colData(scaRaw)$cngeneson <- scale(cdr)
+  #filterCrit = with(colData(scaRaw), cngeneson > 1)
+  rowData(scaRaw)$symbolid <- rownames(rowData(sce))
+  Dose <- factor(colData(scaRaw)$Dose)
+  Dose <- relevel(Dose,"0")
   colData(scaRaw)$Dose = Dose
   zlmDose = zlm(~Dose + cngeneson, sca = scaRaw)
   
-  summaryDose<-rep(list(list()),times= nlevels(Dose)-1)
-  names(summaryDose)<-levels(Dose)[2:9]
-  for(i in levels(Dose)[2:9])
+  summaryDose <- rep(list(list()), times= nlevels(Dose)-1)
+  names(summaryDose) <- levels(Dose)[-1]
+  for(i in levels(Dose)[-1])
   {
-    summaryDose[[i]]<-summary(zlmDose, doLRT=paste0("Dose",i))
+    summaryDose[[i]] <- summary(zlmDose, doLRT=paste0("Dose",i))
   }
   
-  summaryDT<-rep(list(data.table()),times=nlevels(Dose)-1)
-  names(summaryDT)<-levels(Dose)[2:9]
-  fcHurdle<-rep(list(data.table()),times=nlevels(Dose)-1)
-  names(fcHurdle)<-levels(Dose)[2:9]
-  fcHurdleSig<-rep(list(data.table()),times=nlevels(Dose)-1)
-  names(fcHurdleSig)<-levels(Dose)[2:9]
-  fcHurdleNonSig<-rep(list(data.table()),times=nlevels(Dose)-1)
-  names(fcHurdleSig)<-levels(Dose)[2:9]
-  TP_dose<-rep(list(data.table()),times=nlevels(Dose)-1)
-  names(TP_dose)<-levels(Dose)[2:9]
-  FP_dose<-rep(list(data.table()),times=nlevels(Dose)-1)
-  names(FP_dose)<-levels(Dose)[2:9]
-  TN_dose<-rep(list(data.table()),times=nlevels(Dose)-1)
-  names(TN_dose)<-levels(Dose)[2:9]
-  FN_dose<-rep(list(data.table()),times=nlevels(Dose)-1)
-  names(FN_dose)<-levels(Dose)[2:9]
+  summaryDT <- rep(list(data.table::data.table()),times=nlevels(Dose)-1)
+  names(summaryDT) <- levels(Dose)[-1]
+  fcHurdle <- rep(list(data.table::data.table()),times=nlevels(Dose)-1)
+  names(fcHurdle) <- levels(Dose)[-1]
+  fcHurdleSig <- rep(list(data.table::data.table()),times=nlevels(Dose)-1)
+  names(fcHurdleSig) <- levels(Dose)[-1]
+  fcHurdleNonSig <- rep(list(data.table::data.table()),times=nlevels(Dose)-1)
+  names(fcHurdleSig) <- levels(Dose)[-1]
+  TP_dose <- rep(list(data.table::data.table()),times=nlevels(Dose)-1)
+  names(TP_dose) <- levels(Dose)[-1]
+  FP_dose <- rep(list(data.table::data.table()),times=nlevels(Dose)-1)
+  names(FP_dose) <- levels(Dose)[-1]
+  TN_dose <- rep(list(data.table::data.table()),times=nlevels(Dose)-1)
+  names(TN_dose) <- levels(Dose)[-1]
+  FN_dose <- rep(list(data.table::data.table()),times=nlevels(Dose)-1)
+  names(FN_dose) <- levels(Dose)[-1]
   
-  for(i in levels(Dose)[2:9]){
-    summaryDT[[i]] <-summaryDose[[i]]$datatable
-    fcHurdle[[i]]<-summaryDT[[i]][contrast== paste0("Dose",i) & component=='H',.(primerid, `Pr(>Chisq)`)] #logFC coefficients
+  for(i in levels(Dose)[-1]){
+    summaryDT[[i]] <- summaryDose[[i]]$datatable
+    fcHurdle[[i]] <- summaryDT[[i]][contrast == paste0("Dose",i) & component=='H',.(primerid, `Pr(>Chisq)`)] #logFC coefficients
     fcHurdle[[i]][,fdr:=p.adjust(`Pr(>Chisq)`, 'fdr')]
-    fcHurdleSig[[i]]<- fcHurdle[[i]][fdr<.05 ]
+    fcHurdleSig[[i]] <- fcHurdle[[i]][fdr<.05 ]
     setorder(fcHurdleSig[[i]], fdr)
-    fcHurdleNonSig[[i]]<- fcHurdle[[i]][fdr >.05| fdr == 0.05]
+    fcHurdleNonSig[[i]] <- fcHurdle[[i]][fdr >.05| fdr == 0.05]
     setorder(fcHurdleNonSig[[i]], fdr)
   }
   
