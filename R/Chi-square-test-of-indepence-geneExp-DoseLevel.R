1ea55c39ea7c9c4e8a1bbf393d3726b402644bd1
Jack Dodson <dodsonj9901@gmail.com>
2021-05-06 11:14:00 -0400

    Reformatted chi-square

--- R/Chi-square-test-of-indepence-geneExp-DoseLevel.R
+++ R/Chi-square-test-of-indepence-geneExp-DoseLevel.R
@@ -6,35 +6,38 @@
 #' @export
 runChi <- function(sce){
   # TODO: replace hardcoded values.
-  data_logcounts = as.matrix(logcounts(sce))
-  dose = colData(sce)$Dose
-  data_dose<-data.frame(t(data_logcounts),dose)
-  colnames(data_dose)<-c(rownames(data_logcounts),"Dose")
+  data_logcounts <- as.matrix(logcounts(sce))
+  dose <- colData(sce)$Dose
+  data_dose <- data.frame(t(data_logcounts),dose)
+  colnames(data_dose) <- c(rownames(data_logcounts),"Dose")
   #Break the dataframe to data lists for seperate dose groups
-  data = split_tibble(data_dose,"Dose")
-  data<-lapply(data, function(x) t(x))
-  max_value_data<-max(sapply(data, function(x) apply(x,1,max)))
+  data <- split_tibble(data_dose,"Dose")
+  data <- lapply(data, function(x) t(x))
+  max_value_data <- max(sapply(data, function(x) apply(x,1,max)))
   #Create a data list with genes binned into certain values
-  data_bin<-lapply(data, function(x) t(apply(x,1,function(x){
-    new_breaks<-c(-0.01,0.5,1,2,3,4,ceiling(max_value_data)) #Most data exists in these bins - fixed number of bins spanning range of values but needs to be unequal to be more around where most of the data is. Quantiles maybe, but fixed for all dose groups.
+  data_bin <- lapply(data, function(x) t(apply(x,1,function(x){
+    #Most data exists in these bins - fixed number of bins spanning range of 
+    #values but needs to be unequal to be more around where most of the data is. 
+    #Quantiles maybe, but fixed for all dose groups.
+    new_breaks <- c(-0.01,0.5,1,2,3,4,ceiling(max_value_data)) 
     table(cut(x, new_breaks))
   })))
   #Create a list of data frames for applying chi-square test of independence
-  df<-rep(list(data.frame()),length=nrow(data_bin[["0"]]))
-  names(df)<-nrow(data_bin[["0"]])
+  df <- rep(list(data.frame()),length=nrow(data_bin[["0"]]))
+  names(df) <- nrow(data_bin[["0"]])
   for(j in 1:nrow(data_bin[["0"]]))
   {
     a1 <- rapply(data_bin, classes = 'matrix', how = 'list', f = function(x) x[j, , drop = FALSE]) 
     # a second `lapply` is required to drop `NULL` entries
     a1Only <- lapply(a1, Filter, f = Negate(is.null))
     df[[j]] <- data.frame(matrix(unlist(a1Only), nrow=length(a1Only), byrow=TRUE))
-    rownames(df[[j]])<-levels(dose)
-    colnames(df[[j]])<-colnames(data_bin[["0.01"]])
+    rownames(df[[j]]) <- levels(dose)
+    colnames(df[[j]]) <- colnames(data_bin[["0.01"]])
   }
-  chisq_test<-lapply(df,function(x) chisq.test(x[,which(colSums(x)>0)]))
-  names(chisq_test)<-rownames(data[["0"]])
-  chisq_test_pvalue_adj<-p.adjust(sapply(chisq_test,function(x) x$p.value),method = "fdr")
-  chisq_test_pvalue_adj = data.frame(chisq_test_pvalue_adj)
+  chisq_test <- lapply(df,function(x) chisq.test(x[,which(colSums(x)>0)]))
+  names(chisq_test) <- rownames(data[["0"]])
+  chisq_test_pvalue_adj <- p.adjust(sapply(chisq_test,function(x) x$p.value),method = "fdr")
+  chisq_test_pvalue_adj <- data.frame(chisq_test_pvalue_adj)
   return(chisq_test_pvalue_adj)
 }
 
