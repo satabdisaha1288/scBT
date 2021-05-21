0df2b556588fdda707804c17919010ef9404d421
Satabdi Saha <satabdisaha1288@gmail.com>
2021-05-21 15:44:01 -0400

    Update Chi-square-test-of-indepence-geneExp-DoseLevel.R

--- R/Chi-square-test-of-indepence-geneExp-DoseLevel.R
+++ R/Chi-square-test-of-indepence-geneExp-DoseLevel.R
@@ -1,7 +1,7 @@
 #' Chi-square test of independence for binned gene expression data
 #' @author Satabdi Saha
-#' @param sce A Single Cell Object 
-#' @return FDR adjusted p-values 
+#' @param sce A Single Cell Object
+#' @return FDR adjusted p-values
 #
 #' @export
 runChi <- function(sce){
@@ -16,10 +16,10 @@ runChi <- function(sce){
   max_value_data <- max(sapply(data, function(x) apply(x,1,max)))
   #Create a data list with genes binned into certain values
   data_bin <- lapply(data, function(x) t(apply(x,1,function(x){
-    #Most data exists in these bins - fixed number of bins spanning range of 
-    #values but needs to be unequal to be more around where most of the data is. 
+    #Most data exists in these bins - fixed number of bins spanning range of
+    #values but needs to be unequal to be more around where most of the data is.
     #Quantiles maybe, but fixed for all dose groups.
-    new_breaks <- c(-0.01,0.5,1,2,3,4,ceiling(max_value_data)) 
+    new_breaks <- c(-0.01,0.5,1,2,3,4,ceiling(max_value_data))
     table(cut(x, new_breaks))
   })))
   #Create a list of data frames for applying chi-square test of independence
@@ -27,7 +27,7 @@ runChi <- function(sce){
   names(df) <- nrow(data_bin[["0"]])
   for(j in 1:nrow(data_bin[["0"]]))
   {
-    a1 <- rapply(data_bin, classes = 'matrix', how = 'list', f = function(x) x[j, , drop = FALSE]) 
+    a1 <- rapply(data_bin, classes = 'matrix', how = 'list', f = function(x) x[j, , drop = FALSE])
     # a second `lapply` is required to drop `NULL` entries
     a1Only <- lapply(a1, Filter, f = Negate(is.null))
     df[[j]] <- data.frame(matrix(unlist(a1Only), nrow=length(a1Only), byrow=TRUE))
@@ -35,8 +35,9 @@ runChi <- function(sce){
     colnames(df[[j]]) <- colnames(data_bin[["0.01"]])
   }
   #chisq_test<-lapply(df,function(x) chisq.test(x[,which(colSums(x)>0)]))
-  chisq_test <- lapply(df,function(x) chisq.test(x[, which(colSums(x) > 0)]), 
-                       simulate.p.value = TRUE)
+  chisq_test <- lapply(df,function(x) chisq.test(x[, which(colSums(x) > 0)],
+                                                 simulate.p.value = TRUE))
+
   names(chisq_test)<-rownames(data[["0"]])
   chisq_test_pvalue_adj<-p.adjust(sapply(chisq_test,function(x) x$p.value),method = "fdr")
   chisq_test_pvalue_adj = data.frame(chisq_test_pvalue_adj)
@@ -46,8 +47,8 @@ runChi <- function(sce){
 
 #' Chi-square test of independence for binned gene expression data
 #' @author Satabdi Saha
-#' @param sce A Single Cell Object 
-#' @return FDR adjusted p-values 
+#' @param sce A Single Cell Object
+#' @return FDR adjusted p-values
 #
 #' @export
 split_tibble <- function(tibble, column = 'col') {
