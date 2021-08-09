7699999640ef30b887218ca645bb6858eea20079
Nault, Rance <naultran@msu.edu>
2021-08-09 17:52:36 -0400

    fixed Seurat FDR correction

--- R/SeuratBimod.R
+++ R/SeuratBimod.R
@@ -1,10 +1,10 @@
 #' Performs a genewise Wilcoxon Rank Sum test on a SingleCellExperiment object
-#' 
-#' @param sce SingleCellExperiment object with a logcounts assay 
+#'
+#' @param sce SingleCellExperiment object with a logcounts assay
 #' and Dose column in the cell metadata
-#' 
+#'
 #' @return a vector of p values from the Wilcoxon Rank Sum test
-#' 
+#'
 #' @import Seurat
 #' @export
 runSeuratBimod <- function(sce){
@@ -23,19 +23,25 @@ runSeuratBimod <- function(sce){
                                                 min.cell.features = -Inf,
                                                 min.cell.groups = -Inf
                                                 )
-    colnames(res.list[[doseName]]) <- paste0(doseName, 
-                                             '_', 
+    colnames(res.list[[doseName]]) <- paste0(doseName,
+                                             '_',
                                              colnames(res.list[[doseName]])
                                              )
-    
-    
+
+
 
   }
   seur.bimod <- Reduce(merge, lapply(res.list, function(x) data.frame(x, rn = row.names(x))))
   rownames(seur.bimod) <- seur.bimod$rn
   seur.bimod <- seur.bimod[rownames(seurat.data), ]
-  p.val.adj <- seur.bimod[,grepl('p_val_adj', colnames(seur.bimod))]
-  seur.bimod$adjusted.p <- apply(p.val.adj,
-                                 1, function(x) min(x))
+  p.val <- seur.bimod[,grepl('p_val$', colnames(seur.bimod))]
+  seur.bimod$adjusted.p <- apply(p.val,
+                                 1, function(x) p.adjust(min(x), 'fdr')
+  )
+
+  #p.val.adj <- seur.bimod[,grepl('p_val_adj', colnames(seur.bimod))]
+  #seur.bimod$adjusted.p <- apply(p.val.adj,
+  #                               1, function(x) min(x))
+
   return(seur.bimod)
 }
